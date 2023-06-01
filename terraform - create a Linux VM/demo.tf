f06d0d9fb342f9931afeaaa27c7a75a3a68db880
Zulfikar Muhamad <zul.m@icloud.com>
2023-06-01 15:41:58 +0800

    Removed sensitive datas

--- terraform - create a Linux VM/demo.tf
+++ terraform - create a Linux VM/demo.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
   # If you are using version 1.x, the "features" block is not allowed.
   features {}
 
-  subscription_id = "ea17f9bb-2449-44e4-b111-d65ab460d3e0"
-  tenant_id       = "91700184-c314-4dc9-bb7e-a411df456a1e"
+  subscription_id = "YOUR_SUBSCRIPTION_ID"
+  tenant_id       = "YOUR_TENANT_ID"
 
 }
 
